de9c984390c6917e9c6a24549aeb0c0a0f0622fb
maxischiavina <maxischiavina@hotmail.com>
2023-04-07 07:29:05 +0000

    segundo commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const palo = ["♥", "♠", "♦", "♣"];
+  const numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function figuraRandom() {
+    let paloRandom = Math.floor(Math.random() * palo.length);
+    return palo[paloRandom];
+  }
+
+  function numRandom() {
+    let numeroRandom = Math.floor(Math.random() * numero.length);
+    return numero[numeroRandom];
+  }
+
+  let paloFinal = figuraRandom();
+
+  document.getElementById("paloArriba").innerHTML = paloFinal;
+  document.getElementById("numeroCentral").innerHTML = numRandom();
+  document.getElementById("paloAbajo").innerHTML = paloFinal;
 };
